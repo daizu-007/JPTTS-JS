fd6101a5a19dbb5081a9aca84b6e39997c70a913
daizu-007 <128668088+daizu-007@users.noreply.github.com>
2025-04-06 00:59:52 +0900

    話者一覧を取得できるように変更

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
+// index.ts
+// 最終的にはここがエントリーポイントになるが、しばらくは実験場になる。
 import voicevox from './services/voicevox';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
-voicevox
-  .tts('こんにちは、世界！', 0)
-  .then((audioData: ArrayBuffer) => {
+// これでpythonのinput()と同じように使えるらしい
+const rl = readline.createInterface({ input, output });
+
+async function main() {
+  while (true) {
+    console.log('以下のリストから話者を選択してください。');
+    const speakers = await voicevox.fetchSpeakers();
+
+    speakers.forEach((speaker: any) => {
+      console.log(`${speaker.speaker_id} ${speaker.name}`);
+    });
+
+    const speakerId = await rl.question('話者IDを入力してください: ');
+
+    const audioData = await voicevox.tts('こんにちは、世界！', parseInt(speakerId));
     Bun.write('output.wav', audioData); // 書き込み先のファイル名を指定
-  })
-  .catch((error: Error) => {
-    console.error('Error during TTS:', error);
-  });
+    await Bun.spawn(['powershell', '-c', "(New-Object Media.SoundPlayer 'output.wav').PlaySync()"]).exited;
+
+    if ((await rl.question('続けますか？ (y/n): ')) !== 'y') {
+      console.log('終了します。');
+      rl.close();
+      return;
+    }
+  }
+}
+
+main().catch(console.error);
--- src/services/voicevox.ts
+++ src/services/voicevox.ts
@@ -1,51 +1,89 @@
 // voicevox.ts
 
-export default {
-  tts,
-};
+const voicevox = { tts, fetchSpeakers };
+export default voicevox;
+
+// 定数定義
+const VOICEVOX_API_BASE_URL = 'http://localhost:50021';
+const CONTENT_TYPE_JSON = 'application/json';
+let cache_speakers: Array<{
+  speaker_id: number;
+  name: string;
+}> | null = null;
 
 // 音声合成用のクエリを作成する関数
 async function fetchAudioQuery(text: string, speaker: number): Promise<any> {
-  const url = `http://localhost:50021/audio_query?text=${encodeURIComponent(text)}&speaker=${speaker}`;
+  const url = `${VOICEVOX_API_BASE_URL}/audio_query?text=${encodeURIComponent(text)}&speaker=${speaker}`;
   const response = await fetch(url, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': CONTENT_TYPE_JSON,
     },
   });
   if (!response.ok) {
-    throw new Error(
-      `API error in VOICEVOX's AudioQuery call: ${response.status}`
-    );
+    throw new Error(`API error in VOICEVOX's AudioQuery call: ${response.status}`);
   }
   return response.json(); // JSONレスポンスを取得
 }
 
 // 音声合成を行う関数
-async function fetchAudio(text: string, speaker: number): Promise<ArrayBuffer> {
-  // 事前に音声合成用のクエリを作成
-  const query = await fetchAudioQuery(text, speaker);
-
-  // 音声合成リクエスト
-  const url = `http://localhost:50021/synthesis?speaker=${speaker}`; // speakerを動的に設定
+async function fetchAudioData(query: any, speaker: number): Promise<ArrayBuffer> {
+  const url = `${VOICEVOX_API_BASE_URL}/synthesis?speaker=${speaker}`;
   const response = await fetch(url, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': CONTENT_TYPE_JSON,
     },
     body: JSON.stringify(query),
   });
   if (!response.ok) {
-    throw new Error(
-      `API error in VOICEVOX's SynthesisAudio call: ${response.status}`
-    );
+    throw new Error(`API error in VOICEVOX's SynthesisAudio call: ${response.status}`);
   }
-
   return response.arrayBuffer(); // バイナリデータをArrayBufferとして取得
+}
+
+// 話者のリストを取得する関数
+async function fetchSpeakers(forceRefresh?: boolean): Promise<Array<{ speaker_id: number; name: string }>> {
+  const shouldForceRefresh = forceRefresh ?? false;
+  // キャッシュがある場合はそれを返す
+  if (cache_speakers !== null && !shouldForceRefresh) {
+    return cache_speakers;
+  }
+  // キャッシュがない場合はAPIを呼び出す
+  const url = `${VOICEVOX_API_BASE_URL}/speakers`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': CONTENT_TYPE_JSON,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`API error in VOICEVOX's Speakers call: ${response.status}`);
+  }
+  const speakersResponse = (await response.json()) as Array<{
+    name: string;
+    speaker_uuid: string;
+    styles: Array<{ name: string; id: number; type: string }>;
+    version: string;
+    supported_features: { permitted_synthesis_morphing: string };
+  }>;
+  const speakers: Array<{ speaker_id: number; name: string }> = [];
+  speakersResponse.forEach((speaker) => {
+    speaker.styles.forEach((style) => {
+      speakers.push({
+        speaker_id: style.id,
+        name: `${speaker.name} (${style.name})`,
+      });
+    });
+  });
+  // キャッシュに保存
+  cache_speakers = speakers;
+  return speakers;
 }
 
 // 音声合成エンドポイント
 async function tts(text: string, speaker: number): Promise<ArrayBuffer> {
-  const audioData = await fetchAudio(text, speaker);
+  const audioQuery = await fetchAudioQuery(text, speaker);
+  const audioData = await fetchAudioData(audioQuery, speaker);
   return audioData;
 }
